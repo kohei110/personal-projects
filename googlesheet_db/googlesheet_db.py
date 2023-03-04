b4b9045995f3d3109e1f128c3b9fd1daf6bc2b11
kohei110 <kohei.nishitani@gmail.com>
2023-03-04 15:11:24 -0800

    config

--- googlesheet_db/googlesheet_db.py
+++ googlesheet_db/googlesheet_db.py
@@ -1,6 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+import yaml
 
 ## Step1 enable API at GCP
 ## https://console.cloud.google.com/apis/api/sheets.googleapis.com/overview 
@@ -12,86 +13,109 @@ import os
 ## Step3 Get Service account from json key file and add it to as gspreadsheet share account
 ## SERVICE_ACCOUNT@PROJECT.iam.gserviceaccount.com
 
-API_SCOPE1 = 'https://www.googleapis.com/auth/spreadsheets'
-API_SCOPE2 = 'https://www.googleapis.com/auth/drive'
-PATH_TO_JSON = '/Users/nkohei/Workspace/personal-projects/auth/teak-spot-379405-85d7b008753b.json'
-WORKBOOK_NAME = 'test_sheetdb_123'
-SHEET_NAME = 'sheet_xyz'
-EMAIL = 'kohei.nishitani@gmail.com'
-
 class ApiSetting:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     def api_scope(self, apiscope_list: list):
         # create api list
         api_scope = apiscope_list
-
         # connect to json key
-        credentials_path = os.path.join(os.path.expanduser('~'), PATH_TO_JSON)
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, api_scope)
-        return credentials
+        try:
+            credentials_path = os.path.join(os.path.expanduser('~'), PATH_TO_JSON)
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, api_scope)
+            return credentials
+        except Exception as e:
+            print(f"Failed to get credentials: {str(e)}")
+            return None
+
 
 class GoogleSpreadsheetHandler:
     def __init__(self)->None:
         pass
 
     def authorize(self, credential):
-    # authorise
-        gspread_client = gspread.authorize(credential)
-        return gspread_client
+        # authorise
+        try:
+            gspread_client = gspread.authorize(credential)
+            return gspread_client
+        except Exception as e:
+            print(f"Failed to authorise: {str(e)}")
+            return None
 
     def check_workbooks(self, gspread_client, workbookname):
-        workbook_list = []
-        for i in gspread_client.openall():
-            workbook_list.append(i.title)
-        # print(workbook_list)
-        if workbookname in workbook_list:
-            return True
-        else:
-            return False
+        try:
+            workbook_list = []
+            for i in gspread_client.openall():
+                workbook_list.append(i.title)
+
+            if workbookname in workbook_list:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f"Failed to check workbook: {str(e)}")
+            return None
 
     def create_workbook(self, gspread_client ,workbookname):
-        if self.check_workbooks(gspread_client, workbookname):
-            pass
-        else:
-            if workbookname not in [sh.title for sh in gspread_client.openall()]:
-                sh = gspread_client.create(workbookname)
-                sh.share(EMAIL, perm_type='user', role='writer')
+        try:
+            if self.check_workbooks(gspread_client, workbookname):
+                pass
+            else:
+                if workbookname not in [sh.title for sh in gspread_client.openall()]:
+                    sh = gspread_client.create(workbookname)
+                    sh.share(EMAIL, perm_type='user', role='writer')
 
-            print('new workbook created, please check email')
+                print('new workbook created, please check email')
+        except Exception as e:
+            print(f"Failed to create workbook {str(e)}")
+            return None
 
     def check_worksheets(self,gspread_client, workbookname, sheetname):
-        sh = gspread_client.open(workbookname)
-        worksheet_list = sh.worksheets()
-
-        if sheetname in worksheet_list:
-            return True
-        else:
-            return False
+        try:
+            sh = gspread_client.open(workbookname)
+            worksheet_list = sh.worksheets()
+            if sheetname in worksheet_list:
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f"Failed to check worksheet: {str(e)}")
+            return None
 
     def creat_worksheet(self, gsspread_client, workbookname, sheetname):
-        if self.check_worksheets(gsspread_client, workbookname, sheetname):
-            pass
-        else:
-            sh = gspread_client.open(workbookname)
-            worksheet = sh.add_worksheet(title=sheetname, rows=100, cols=20)
-            print('new sheet created')
+        try:
+            if self.check_worksheets(gsspread_client, workbookname, sheetname):
+                pass
+            else:
+                sh = gspread_client.open(workbookname)
+                worksheet = sh.add_worksheet(title=sheetname, rows=100, cols=20)
+                print('new sheet created')
+        except Exception as e:
+            print(f"Failed to create worksheet: {str(e)}")
+            return None
 
-# Usage example
-api = ApiSetting()
-credentials = api.api_scope([API_SCOPE1,API_SCOPE2])
-gs_handler = GoogleSpreadsheetHandler()
-gspread_client = gs_handler.authorize(credentials)
+if __name__ == '__main__':
+    # read configuration
+    with open('config.yml', 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
-# create
-gs_handler.create_workbook(gspread_client, WORKBOOK_NAME)
+    PATH_TO_JSON = config['PATH_TO_JSON']
+    WORKBOOK_NAME = config['WORKBOOK_NAME']
+    SHEET_NAME = config['SHEET_NAME']
+    API_SCOPE = config['API_SCOPE']
+    EMAIL = config['EMAIL']
 
-# sheet
-gs_handler.creat_worksheet(gspread_client, WORKBOOK_NAME, SHEET_NAME)
+    # Usage example
+    api = ApiSetting()
+    credentials = api.api_scope(API_SCOPE)
+    gs_handler = GoogleSpreadsheetHandler()
+    gspread_client = gs_handler.authorize(credentials)
 
+    # create
+    gs_handler.create_workbook(gspread_client, WORKBOOK_NAME)
 
-#checking
+# checking
 # gs_handler.check_workbooks(gspread_client, SHEET_NAME)
 
 # # # open sheet
