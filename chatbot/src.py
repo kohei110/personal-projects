787dc89c395e7ba357f937f8dbb903cc1caf8d42
kohei110 <kohei.nishitani@gmail.com>
2023-06-23 15:53:12 -0700

    env file add

--- chatbot/src.py
+++ chatbot/src.py
@@ -3,6 +3,6 @@ import openai
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) 
 
-openai.api_key  = os.environ['APIKEY']
+openai.api_key  = os.environ['OAI_APIKEY']
 
 print(openai.Engine.list())
